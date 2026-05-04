6f7d89047c7a0fc4f7ca4fd53f7990d0e9104e55
GDCorona <phanthethanhson@gmail.com>
2026-05-04 23:40:09 +0700

    small update

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,8 @@ const commentSchema = new mongoose.Schema({
   email: { type: String, required: true },
   text: { type: String, required: true },
   page: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
+  isPinned: { type: Boolean, default: false }
 });
 
 export default mongoose.model("Comment", commentSchema);
